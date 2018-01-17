22375597eb03056e5bdebef20e7d67bc2e55248b
wslsq <flofire.l@gmail.com>
2018-01-17 10:02:04 +0800

    Update JdClient.php

--- src/jos/JdClient.php
+++ src/jos/JdClient.php
@@ -171,12 +171,14 @@ class JdClient
 		//如果JD返回了错误码，记录到业务错误日志中
 		if (isset($respObject->code))
 		{
+			/*
 			$logger = new LtLogger;
 			$logger->conf["log_file"] = rtrim(JD_SDK_WORK_DIR, '\\/') . '/' . "logs/top_biz_err_" . $this->appKey . "_" . date("Y-m-d") . ".log";
 			$logger->log(array(
 				date("Y-m-d H:i:s"),
 				$resp
 			));
+			*/
 		}
 		return $respObject;
 	}
